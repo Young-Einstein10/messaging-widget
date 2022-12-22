e8091af278a4f687b61ff6d9af5b693549dce534
Abdulrahman Yusuf <aabdulrahmanyusuf@gmail.com>
2022-12-23 00:12:26 +0100

    feature: cimplete widget implementation

---
+++ assets.js
@@ -0,0 +1,126 @@
+export const styles = `
+    .widget__container * {
+        box-sizing: border-box;
+    }        
+
+    h3, p, input {
+        margin: 0;
+        padding: 0;
+    }
+
+    .widget__container {
+        box-shadow: 0 0 18px 8px rgba(0, 0, 0, 0.1), 0 0 32px 32px rgba(0, 0, 0, 0.08);
+        width: 400px;
+        overflow: auto;
+        right: -25px;
+        bottom: 75px;
+        position: absolute;
+        transition: max-height .2s ease;
+        font-family: Helvetica, Arial ,sans-serif;
+        background-color: #e6e6e6a6;
+        border-radius: 10px;
+        box-sizing: border-box;
+    }
+
+    .widget__icon {
+        cursor: pointer;
+        width: 60%;
+        position: absolute;
+        top: 18px;
+        left: 16px;
+        transition: transform .3s ease;
+    }
+
+    .widget__hidden {
+        transform: scale(0);
+    }
+    .button__container {
+        border: none;
+        background-color: #0f172a;
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        cursor: pointer;
+    }
+
+    .widget__container.hidden {
+        max-height: 0px;
+    }
+
+    .widget__header {
+        padding: 1rem 2rem 1.5rem;
+        background-color: #000;
+        color: #fff;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+        text-align: center;
+    }
+
+    .widget__header h3 {
+        font-size: 24px;
+        font-weight: 400;
+        margin-bottom: 8px;
+    }
+
+    form {
+        padding: 2rem 1rem 1.5rem;
+    }
+
+    form .form__field {
+        margin-bottom: 1.5rem;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .form__field label {
+        margin-bottom: 8px;
+        font-size: 14px;
+    }
+
+    .form__field input,
+    .form__field textarea {
+        border: 1px solid #000000ad;
+        border-radius: 3px;
+        padding: 8px 10px;
+        background-color: #fff;
+    }
+
+    .form__field input {
+        height: 48px;
+    }
+
+    .form__field textarea::placeholder {
+        font-family: Helvetica, Arial ,sans-serif;
+    }
+
+    form button {
+        height: 48px;
+        border-radius: 6px;
+        font-size: 18px;
+        background-color: #000;
+        color: #fff;
+        border: 0;
+        width: 100%;
+        cursor: pointer;
+    }
+
+    form button:hover {
+        background-color: rgba(0, 0, 0, 95%);
+    }
+`;
+
+export const MESSAGE_ICON = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none" stroke="#FFFFFF"
+        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
+        <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+        <polyline points="22,6 12,13 2,6"></polyline>
+    </svg>
+`;
+
+export const CLOSE_ICON = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="#FFFFFF" stroke="#FFFFFF"
+        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+        <line x1="18" y1="6" x2="6" y2="18"></line>
+        <line x1="6" y1="6" x2="18" y2="18"></line>
+    </svg>
+`;
---
+++ main.js
@@ -0,0 +1,160 @@
+import { CLOSE_ICON, MESSAGE_ICON, styles } from "./assets.js";
+
+class MessageWidget {
+  constructor(position = "bottom-right") {
+    this.position = this.getPosition(position);
+    this.open = false;
+    this.initialize();
+    this.injectStyles();
+  }
+
+  position = "";
+  open = false;
+  widgetContainer = null;
+
+  getPosition(position) {
+    const [vertical, horizontal] = position.split("-");
+    return {
+      [vertical]: "30px",
+      [horizontal]: "30px",
+    };
+  }
+
+  async initialize() {
+    /**
+     * Create and append a div element to the document body
+     */
+    const container = document.createElement("div");
+    container.style.position = "fixed";
+    Object.keys(this.position).forEach(
+      (key) => (container.style[key] = this.position[key])
+    );
+    document.body.appendChild(container);
+
+    /**
+     * Create a button element and give it a class of button__container
+     */
+    const buttonContainer = document.createElement("button");
+    buttonContainer.classList.add("button__container");
+
+    /**
+     * Create a span element for the widget icon, give it a class of 'widget__icon', update it's innerHTML property to an icon which would serve as the widget icon.
+     */
+    const widgetIconElement = document.createElement("span");
+    widgetIconElement.innerHTML = MESSAGE_ICON;
+    widgetIconElement.classList.add("widget__icon");
+    this.widgetIcon = widgetIconElement;
+
+    /**
+     * Create a span element for the close icon, give it a class of 'widget__icon' and 'widget__hidden' which would be removed whenever the widget is closed, update it's innerHTML property to an icon which would serve as the widget icon during that state.
+     */
+    const closeIconElement = document.createElement("span");
+    closeIconElement.innerHTML = CLOSE_ICON;
+    closeIconElement.classList.add("widget__icon", "widget__hidden");
+    this.closeIcon = closeIconElement;
+
+    /**
+     * Append both icons created to the button element and add a `click` event listener on the button to toggle the widget open and close.
+     */
+    buttonContainer.appendChild(this.widgetIcon);
+    buttonContainer.appendChild(this.closeIcon);
+    buttonContainer.addEventListener("click", this.toggleOpen.bind(this));
+
+    /**
+     * Create a container for the widget and add the following classes:- "widget__hidden", "widget__container"
+     */
+    this.widgetContainer = document.createElement("div");
+    this.widgetContainer.classList.add("widget__hidden", "widget__container");
+
+    /**
+     * Invoke the `createWidget()` method
+     */
+    this.createWidgetContent();
+
+    /**
+     * Append the widget's content and the button to the container
+     */
+    container.appendChild(this.widgetContainer);
+    container.appendChild(buttonContainer);
+  }
+
+  createWidgetContent() {
+    this.widgetContainer.innerHTML = `
+        <header class="widget__header">
+            <h3>Start a conversation</h3>
+            <p>We usually respond within a few hours</p>
+        </header>
+
+        <form>
+            <div class="form__field">
+                <label for="name">Name</label>
+                <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Enter your name"
+                />
+            </div>
+
+            <div class="form__field">
+                <label for="email">Email</label>
+                <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                />
+            </div>
+
+            <div class="form__field">
+                <label for="subject">Subject</label>
+                <input
+                type="text"
+                id="subject"
+                name="subject"
+                placeholder="Enter Message Subject"
+                />
+            </div>
+
+            <div class="form__field">
+                <label for="message">Message</label>
+                <textarea
+                id="message"
+                name="message"
+                placeholder="Enter your message"
+                rows="6"
+                ></textarea>
+            </div>
+
+            <button>Send Message</button>
+        </form>
+    `;
+  }
+
+  injectStyles() {
+    const styleTag = document.createElement("style");
+    styleTag.innerHTML = styles.replace(/^\s+|\n/gm, "");
+
+    document.head.appendChild(styleTag);
+  }
+
+  toggleOpen() {
+    this.open = !this.open;
+    if (this.open) {
+      this.widgetIcon.classList.add("widget__hidden");
+      this.closeIcon.classList.remove("widget__hidden");
+      this.widgetContainer.classList.remove("widget__hidden");
+    } else {
+      this.createWidgetContent();
+      this.widgetIcon.classList.remove("widget__hidden");
+      this.closeIcon.classList.add("widget__hidden");
+      this.widgetContainer.classList.add("widget__hidden");
+    }
+  }
+}
+
+function initializeWidget() {
+  return new MessageWidget();
+}
+
+initializeWidget();
